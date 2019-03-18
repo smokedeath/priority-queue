2e42807bea30c8d83e37606e2d642711bb834574
smoke <smokedeath87@gmail.com>
2019-03-19 00:49:07 +0600

    work in progress

--- src/node.js
+++ src/node.js
@@ -10,22 +10,22 @@ class Node {
 	appendChild(node) {
 		if (this.left == null) {
 			this.left = node;
-            node.parent = this;
+			node.parent = this;
 		} else
 			if (this.left != null && this.right == null) {
 				this.right = node;
-                node.parent = this;
+				node.parent = this;
 			}
 	}
 
 	removeChild(node) {
 		if (node.data === this.left.data && node.priority === this.left.priority) {
 			this.left = null;
-            node.parent = null;
+			node.parent = null;
 		} else
 			if (node.data === this.right.data && node.priority === this.right.priority) {
 				this.right = null;
-                node.parent = null;
+				node.parent = null;
 			} else {
 					throw "Error";
 				}
