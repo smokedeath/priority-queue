221dcc94ab12fe008b86e7352e508b68b5bb9325
smoke <smokedeath87@gmail.com>
2019-03-19 13:21:16 +0600

    in proggres

--- src/node.js
+++ src/node.js
@@ -1,45 +1,69 @@
 class Node {
-	constructor(data, priority) {
-		this.data = data;
-		this.priority = priority;
+    constructor(data, priority) {
+        this.data = data;
+        this.priority = priority;
         this.parent = null;
         this.left = null;
         this.right = null;
-	}
+    }
 
-	appendChild(node) {
-		if (this.left == null) {
-			this.left = node;
-			node.parent = this;
-		} else
-			if (this.left != null && this.right == null) {
-				this.right = node;
-				node.parent = this;
-			}
-	}
+    appendChild(node) {
+        if (this.left == null) {
+            this.left = node;
+            node.parent = this;
+        } else
+        if (this.left != null && this.right == null) {
+            this.right = node;
+            node.parent = this;
+        }
+    }
 
-	removeChild(node) {
-		if (node.data === this.left.data && node.priority === this.left.priority) {
-			this.left = null;
-			node.parent = null;
-		} else
-			if (node.data === this.right.data && node.priority === this.right.priority) {
-				this.right = null;
-				node.parent = null;
-			} else {
-					throw "Error";
-				}
-	}
+    removeChild(node) {
+        if (node === this.left) {
+            this.left = null;
+            node.parent = null;
+        } else
+        if (node === this.right) {
+            this.right = null;
+            node.parent = null;
+        } else {
+            throw "Error";
+        }
+    }
 
-	remove() {
-		if (this.parent != null) this.parent.removeChild(this);
-	}
+    remove() {
+        if (this.parent != null) this.parent.removeChild(this);
+    }
 
-	swapWithParent() {
-		if (this.parent != null) {
-			//
-		}
-	}
+    swapWithParent() {
+        if (this.parent != null) {
+            let pr = this.parent;
+            let oPr = {};
+            oPr['parent'] = pr.parent;
+            oPr['left'] = pr.left;
+            oPr['right'] = pr.right;
+            let oCl = {};
+            oCl['parent'] = this.parent;
+            oCl['left'] = this.left;
+
+            this.parent = oPr['parent'];
+            pr.parent = this;
+            if (pr.left!=null && pr.left!==this) pr.left.parent = this;
+            if (pr.right!=null && pr.right!==this) pr.right.parent = this;
+            if (this===oPr['left']) {
+                this.left = pr;
+                this.right = pr.right;
+            } else if (this===oPr['right']) {
+                this.left = pr;
+                this.left = pr.left;
+            }
+            pr.left = oCl['left'];
+            pr.right = oCl['right'];
+
+            oPr = null;
+            oCl = null;
+        }
+    }
 }
 
 module.exports = Node;
