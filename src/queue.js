a2e6896d3fff8eb1557bdd657ccf88e627c78f37
smoke <smokedeath87@gmail.com>
2019-03-20 17:41:04 +0600

    in progress

--- src/queue.js
+++ src/queue.js
@@ -2,11 +2,20 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
+		this.heap = new MaxHeap();
+		if (maxSize != null) {
+            this.maxSize = maxSize;
+		} else {
+			this.maxSize = 30;
+		}
 
 	}
 
 	push(data, priority) {
-
+		if (this.heap.parentNodes.length === this.maxSize) {
+            throw 'Error Max Size';
+		}
+		this.heap.push(data, priority);
 	}
 
 	shift() {
